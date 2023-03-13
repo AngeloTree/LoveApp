d580165e0f771e8f8adbbfcac0440d5dbec16e4b
Angelo Chaknis <angelopalmtree@gmail.com>
2023-03-13 15:09:01 -0700

    feat: add love-calculator api

---
+++ API/app.js
@@ -0,0 +1,12 @@
+const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "dded2dedaamsh1d3e249ad8143a1p16c7c5jsn4522a91347b3",
+    "X-RapidAPI-Host": "love-calculator.p.rapidapi.com",
+  },
+};
+
+fetch(
+  "https://love-calculator.p.rapidapi.com/getPercentage?sname=Alice&fname=John'",
+  options
+).then((res) => console.log(res));
